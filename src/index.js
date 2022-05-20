a1275c42d849c69454a3d22022a6da8ecdda7148
jeremiahomueti <jeremiahomueti@gmail.com>
2022-05-20 16:39:10 +0100

    update

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/*here we need to import a couple of objects from react modules.
+Teacher says, if you are not familiar with ES6 modules yet, don't worry, just code along, we'll get to those pretty bunch soon. 
+Alrighty Then!! */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const ELEMENT = <h1> Hello World</h1>;
+ReactDOM.render(ELEMENT, document.getElementById('root'));
